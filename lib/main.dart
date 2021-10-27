f1ba532eb889aecdc631137f0d749df84180b3d0
Parth Darji <parthdarji2961@gmail.com>
2021-10-27 15:37:03 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Material(
+        child: Center(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            // ignore: prefer_const_constructors
+            child: Text("Welcome"),
+          ),
+        ),
+      ),
+    );
+  }
+}
