974380a45157590846be2d53d38237c0c0608534
Parth Darji <parthdarji2961@gmail.com>
2021-11-20 10:11:51 +0530

    Day 15 Completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/store.dart';
 import 'package:flutter_app/models/cart.dart';
 import 'package:flutter_app/widgets/themes.dart';
 import "package:velocity_x/velocity_x.dart";
@@ -26,15 +27,15 @@ class CartPage extends StatelessWidget {
 }
 
 class _CartTotal extends StatelessWidget {
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
+    final CartModel? _cart = (VxState.store as MyStore).cart;
     return SizedBox(
       height: 200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          "\$${_cart.totalPrice}"
+          "\$${_cart!.totalPrice}"
               .text
               .xl5
               .color(context.theme.accentColor)
@@ -61,10 +62,10 @@ class _CartTotal extends StatelessWidget {
 }
 
 class _CartList extends StatelessWidget {
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
-    return _cart.items!.isEmpty
+    final CartModel? _cart = (VxState.store as MyStore).cart;
+    return _cart!.items!.isEmpty
         ? "Nothing to Show".text.xl3.makeCentered()
         : ListView.builder(
             itemBuilder: (context, index) => ListTile(
--- lib/widgets/home_widgets/add_to_cart.dart
+++ lib/widgets/home_widgets/add_to_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/core/store.dart';
 import 'package:flutter_app/models/cart.dart';
 import 'package:flutter_app/models/catalog.dart';
 import "package:velocity_x/velocity_x.dart";
@@ -10,17 +11,16 @@ class AddToCart extends StatelessWidget {
     Key? key,
     required this.catalog,
   }) : super(key: key);
-  final _cart = CartModel();
+
   @override
   Widget build(BuildContext context) {
-    bool isInCart = _cart.items!.contains(catalog);
+    VxState.watch(context, on: [AddMutation]);
+    final CartModel? _cart = (VxState.store as MyStore).cart;
+    bool isInCart = _cart!.items!.contains(catalog);
     return ElevatedButton(
       onPressed: () {
         if (!isInCart) {
-          isInCart = isInCart.toggle();
-          final _catalog = CatalogModel();
-          _cart.catalog = _catalog;
-          _cart.add(catalog);
+          AddMutation(catalog);
         }
       },
       child: isInCart ? Icon(Icons.done) : Icon(CupertinoIcons.cart_badge_plus),
